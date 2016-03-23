dd2f38d3c44c93fdd889ee9bcce5df3ca32931c0
Amy de Buitléir <amy@nualeargais.ie>
2016-03-23 16:32:56 +0000

    added some tests

--- src/ALife/Creatur/Wain/TestUtils.hs
+++ src/ALife/Creatur/Wain/TestUtils.hs
@@ -16,43 +16,70 @@ module ALife.Creatur.Wain.TestUtils
     arb8BitInt,
     prop_serialize_round_trippable,
     prop_genetic_round_trippable,
+    prop_genetic_round_trippable2,
     prop_diploid_identity,
     prop_show_read_round_trippable
   ) where
 
-import qualified ALife.Creatur.Genetics.BRGCWord8 as G
+import qualified ALife.Creatur.Genetics.BRGCWord8 as W8
 import ALife.Creatur.Genetics.Diploid (Diploid, express)
 import ALife.Creatur.Util (fromEither)
 import ALife.Creatur.Wain.Util (scaleFromWord8, scaleWord8ToInt)
+import Control.Monad.State.Lazy (runState)
 import Data.Serialize (Serialize, encode, decode)
 import Data.Word (Word8)
 import Test.QuickCheck
 
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
 arb8BitDouble :: (Double, Double) -> Gen Double
-arb8BitDouble interval = do 
+arb8BitDouble interval = do
   x <- arbitrary :: Gen Word8
   return $ scaleFromWord8 interval x
-  
+
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
 arb8BitInt :: (Int, Int) -> Gen Int
-arb8BitInt interval = do 
+arb8BitInt interval = do
   x <- arbitrary :: Gen Word8
   return $ scaleWord8ToInt interval x
-  
+
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
 prop_serialize_round_trippable :: (Eq a, Serialize a) => a -> Property
 prop_serialize_round_trippable x = property $ x' == Right x
   where bs = encode x
         x' = decode bs
 
-prop_genetic_round_trippable :: (Eq g, G.Genetic g, Show g) =>
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
+prop_genetic_round_trippable :: (Eq g, W8.Genetic g, Show g) =>
   (g -> g -> Bool) -> g -> Property
-prop_genetic_round_trippable equiv g = property $ g' `equiv` g
-  where x = G.write g
-        g' = fromEither (error "read returned Nothing") . G.read $ x
+prop_genetic_round_trippable eq g = property $
+  g' `eq` g && null leftover
+  where x = W8.write g
+        (result, (_, i, _)) = runState W8.get (x, 0, [])
+        leftover = drop i x
+        g' = fromEither (error "read returned Nothing") $ result
 
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
+prop_genetic_round_trippable2
+  :: W8.Genetic g => Int -> [Word8] -> g -> Property
+prop_genetic_round_trippable2 n xs dummy = length xs >= n
+  ==> xs' == take n xs
+  where Right g = W8.read xs
+        xs' = W8.write (g `asTypeOf` dummy)
+
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
 prop_diploid_identity :: Diploid g => (g -> g -> Bool) -> g -> Property
-prop_diploid_identity equiv g = property $ express g g `equiv` g
+prop_diploid_identity eq g = property $ express g g `eq` g
 
-prop_show_read_round_trippable :: (Read a, Show a) => (a -> a -> Bool) -> a -> Property
-prop_show_read_round_trippable equiv x
-  = property $ (read . show $ x) `equiv` x
+-- IMPORTANT: Keep the code for this function in sync with the 
+-- version in creatur-wains
+prop_show_read_round_trippable
+  :: (Read a, Show a) => (a -> a -> Bool) -> a -> Property
+prop_show_read_round_trippable eq x
+  = property $ (read . show $ x) `eq` x
     
