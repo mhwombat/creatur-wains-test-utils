56e8cb4dad837a9767ca056d2103173f4ca7e63a
Amy de Buitléir <amy@nualeargais.ie>
2021-07-27 21:05:34 +0100

    refresh

--- default.nix
+++ default.nix
@@ -1,42 +1,11 @@
-{ nixpkgs ? import ./nix/nixos-20-03.nix }:
-let
-  overlay = self: super: {
-    myHaskellPackages = 
-      super.haskell.packages.ghc865.override (old: {
-        overrides = self.lib.composeExtensions (old.overrides or (_: _: {})) 
-          (hself: hsuper: {
-            ghc = hsuper.ghc // { withPackages = hsuper.ghc.withHoogle; };
-            ghcWithPackages = hself.ghc.withPackages;
-            grid = self.haskell.lib.dontCheck (hself.callCabal2nix "grid" /home/amy/github/grid {});
-            som = self.haskell.lib.dontCheck (hself.callCabal2nix "som" /home/amy/github/som {});
-            creatur = self.haskell.lib.dontCheck (hself.callCabal2nix "creatur" /home/amy/github/creatur {});
-            creatur-wains = self.haskell.lib.dontCheck (hself.callCabal2nix "creatur-wains" /home/amy/github/creatur-wains {});
-            lens = self.haskell.lib.dontCheck 
-              (hself.callHackageDirect 
-                { pkg = "lens";
-                  ver = "4.19.2";
-                  sha256 = "0cgkigb7p0igzg9l669xkq787bb1cw32lx03pcgv5ivd6zsx3fpm";
-                } {}
-              );
-          });
-      });
-  };
-
-  pkgs = import nixpkgs {
-    overlays = [overlay];
-  };
-
-  drv = pkgs.myHaskellPackages.callCabal2nix "creatur-wains-test-utils" ./creatur-wains-test-utils.cabal {};
-
-  drvWithTools = drv.env.overrideAttrs (
-    old: with pkgs.myHaskellPackages; {
-      nativeBuildInputs = old.nativeBuildInputs ++ [
-        ghcid
-        # Add other development tools like ormolu here
-      ];
-      shellHook = ''
-         source .config/secrets     '';
-      }
-  );
-in
-  drvWithTools
+let 
+  pkgs = import <nixpkgs> { };
+in 
+  pkgs.haskellPackages.developPackage {
+    root = ./.;
+    source-overrides = {
+      som = ../som;
+      creatur = ../creatur;
+      creatur-wains = ../creatur-wains;
+    };
+  }
